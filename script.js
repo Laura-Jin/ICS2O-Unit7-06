2c675b22119cee52a6f299f389e08d21eb5fb55c
LauraJin8 <Laura.jin@ocsbstudent.ca>
2021-02-23 15:23:39 +0000

    deleted spaces

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ document.getElementById('button').addEventListener('click', guessButton)
 let age = 15
 
 function guessButton () {
-  let guess = prompt('Enter your guess here', 'ie. 37') 
+  let guess = prompt('Enter your guess here', 'ie. 37')
   guess = parseInt(guess)
   age = parseInt(age)
 
   do {
     if (guess > age) {
-      guess = prompt ('You guessed too big. Guess again.')
+      guess = prompt('You guessed too big. Guess again.')
     } else if (guess < age) {
-      guess = prompt('You guessed too small. Guess again.') 
+      guess = prompt('You guessed too small. Guess again.')
     } else if (guess === age) {
       document.getElementById('text').innerHTML = 'You got it right! Do you want to play again'
     }
